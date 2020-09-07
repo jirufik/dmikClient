f198c3a66b19f0d818878674f68fdeb5c2047ca6
jirufik <rufus57@yandex.ru>
2020-09-07 19:00:37 +0300

    update getImgFilm in utils

--- src/utils/getImgFilm.js
+++ src/utils/getImgFilm.js
@@ -1,7 +1,7 @@
 export default function getImgFilm({film, width = 400, original = false}) {
 
   const img = film.img;
-  if (img.includes('dmik')) return img;
+  if (img.includes('http')) return img;
 
   // return `https://image.tmdb.org/t/p/w200${img}`;
   if (original) return `https://image.tmdb.org/t/p/original${img}`;
